f75a930f1fd8b82077c954c78e35381b626af17b
attiilla <atilappl@gmail.com>
2023-04-15 19:51:10 -0300

    build:Added react to project

---
+++ front_end/src/App.js
@@ -0,0 +1,21 @@
+import bg from './BG.jpg';
+import './App.css';
+
+function App() {
+  const background = {
+    backgroundImage: `url(${bg})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    height: '100vh',
+    width: '100vw'
+  };
+  return (
+    <div style={background}>
+      <div className="App">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
