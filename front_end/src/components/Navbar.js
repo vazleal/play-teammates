04eedadc270f953d59529816af8287fdf41b3b3a
attiilla <atilappl@gmail.com>
2023-04-18 15:59:55 -0300

    style:buttons code justified

--- front_end/src/components/Navbar.js
+++ front_end/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import styles from './Navbar.module.css'
+
 const Navbar = (props) => {
   function toLog(){
     console.log("To the LogIn Page");
@@ -10,12 +11,18 @@ const Navbar = (props) => {
     <>
     <div className={styles.navbarContainer}>
       <div className={styles.logoNavbar}></div>
-        <div className={styles.frameEntrar}>
-        <button className={styles.button} style={{ '--background_color': '#172848' }} onClick={toLog}>Entrar</button>
-        </div>
-        <div className={styles.frameConta}>
-        <button className={styles.button} style={{ '--background_color': '#2b3e5c' }} onClick={toSign}>Conta</button>
-        </div>
+      <div className={styles.frameEntrar}>
+          <button className={styles.button} 
+            style={{ '--background_color': '#172848' }} onClick={toLog}>
+            Entrar
+          </button>
+      </div>
+      <div className={styles.frameConta}>
+          <button className={styles.button} 
+            style={{ '--background_color': '#2b3e5c' }} onClick={toSign}>
+            Conta
+          </button>
+      </div>
     </div>
     </>
   )
