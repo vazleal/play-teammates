f13a6d099b64b2d17d180f8355ffec35b1871681
vazleal <vazleal@outlook.com>
2023-05-09 23:52:29 -0300

    Change create invite icon in navbar and indent archives

--- client/src/components/Background.jsx
+++ client/src/components/Background.jsx
@@ -1,5 +1,4 @@
 import { Container } from '@mui/material'
-import backgroundImage from '../assets/images/background.svg'
 
 function BackgroundImage(props) {
   return <Container maxWidth="xl" {...props} />
